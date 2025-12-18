63ac4444e0c9bd13fc2197eb266fd149cc6d53c9
kundan <kundan8789@gmail.com>
2025-12-18 22:57:56 +0530

    creating dasboad

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/module/auth/utils/auth-utils";
 import Logout from "@/module/auth/components/logout";
-import Image from "next/image";
+import { redirect } from "next/navigation";
+
 
 export default async function Home() {
   await requireAuth()
+  return redirect('/dashboard')
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Logout>
